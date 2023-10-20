47619ae8dd6c32e8640cd4564b85056900abcb83
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-20 13:05:24 +0100

    Query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { PreviewProvider } from './Context/PreviewContext.jsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PreviewProvider>
-    <App />
-    </PreviewProvider>
+    <QueryClientProvider client={queryClient}>
+      <PreviewProvider>
+      <App />
+      </PreviewProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
