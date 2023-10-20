b7c08c285ad2beadc8a2d6b4b16bcbb79ea4f064
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-20 13:09:28 +0100

    Dropdowns added to survey page

--- src/Pages/MySurveys.jsx
+++ src/Pages/MySurveys.jsx
@@ -1,4 +1,5 @@
-
+import CountryDropdown from "../components/Dropdown/CountryDropdown";
+import LocationDropdown from "../components/Dropdown/LocationDropdown";
 
 export default function MySurveys() {
     return (
@@ -23,21 +24,13 @@ export default function MySurveys() {
                                 <div className="sm:col-span-3">
 
                                     <div className="mt-2">
-                                        <select id="country" name="country" autoComplete="country-name" className="block font-bold text-white w-full border-0 py-1.5 shadow-sm   sm:max-w-xs sm:text-sm sm:leading-6 bg-[#FF3131] outline-none ">
-                                            <option>Select country</option>
-                                            <option>Canada</option>
-                                            <option>Mexico</option>
-                                        </select>
+                                        <CountryDropdown/>
                                     </div>
                                 </div>
                                 <div className="sm:col-span-3">
 
                                     <div className="mt-2">
-                                        <select id="country" name="country" autoComplete="country-name" className="block font-bold text-white w-full border-0 py-1.5 shadow-sm  sm:max-w-xs sm:text-sm sm:leading-6 bg-[#FF3131] outline-none">
-                                            <option>Select location</option>
-                                            <option>Canada</option>
-                                            <option>Mexico</option>
-                                        </select>
+                                        <LocationDropdown/>
                                     </div>
                                 </div>
                             </div>
