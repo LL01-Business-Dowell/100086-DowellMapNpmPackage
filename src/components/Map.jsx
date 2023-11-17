830e599386c59c0722d5d80aec8c89baa38abf60
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-17 12:40:56 +0100

    All the markers from the search results rendered

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,62 +1,46 @@
 import GoogleMapReact from "google-map-react";
 import { ImLocation } from "react-icons/im";
-import Marker from "./Marker";
+// import Marker from "./Marker";
+import {APIProvider, Map, Marker, useMapsLibrary, useMarkerRef} from '@vis.gl/react-google-maps';
+import { useCallback, useState } from "react";
 
 export default function MainMap({centerCords, pins}){
-
-  function createMapOptions(maps) {
-    // next props are exposed at maps
-    // "Animation", "ControlPosition", "MapTypeControlStyle", "MapTypeId",
-    // "NavigationControlStyle", "ScaleControlStyle", "StrokePosition", "SymbolPath", "ZoomControlStyle",
-    // "DirectionsStatus", "DirectionsTravelMode", "DirectionsUnitSystem", "DistanceMatrixStatus",
-    // "DistanceMatrixElementStatus", "ElevationStatus", "GeocoderLocationType", "GeocoderStatus", "KmlLayerStatus",
-    // "MaxZoomStatus", "StreetViewStatus", "TransitMode", "TransitRoutePreference", "TravelMode", "UnitSystem"
-    return {
-      zoomControlOptions: {
-        position: maps.ControlPosition.RIGHT_CENTER,
-        style: maps.ZoomControlStyle.SMALL
-      },
-      mapTypeControlOptions: {
-        position: maps.ControlPosition.TOP_RIGHT
-      },
-      mapTypeControl: true
-    };
-  }
-
-
-    const Pins = () => <div style={markerStyles}><ImLocation color="red" size={30}/></div>;
+    const [markerRef, marker] = useMarkerRef();
+    const Circle = useMapsLibrary("Circle");
+    // const [Circle,setCircle ] = useState(null);
 
 
     const defaultProps = {
         center: {
-            lat: centerCords.center_lat?centerCords.center_lat:10.23295,
-            lng: centerCords.center_lon?centerCords.center_lon:10.28572,
+            lat: centerCords?centerCords.center_lat:51.50853,
+            lng: centerCords?centerCords.center_lon:-0.12574,
           },
         zoom: 7,
   };
-  const getLatandLong = (coord_string) => {
-    // const lat_long = useState([]);
-    // console.log("coordinates", coord_string.split(" , "));
-    return coord_string.split(" , ");
-  };
+
+
   return (
     <div style={{ height: "100%", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAsH8omDk8y0lSGLTW9YtZiiQ2MkmsF-uQ" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-        options={createMapOptions}
-      >
-        {/* {pins.map((item) => (
-          <div key={item}>
-            <Pins
-              lat={parseFloat(getLatandLong(item.location_coord)[0])}
-              lng={parseFloat(getLatandLong(item.location_coord)[1])}
-            />
-          </div>
-        ))} */}
-        <Marker lat={51.4409868} lng={-0.06103330000000001} text={"B"}/>
-      </GoogleMapReact>
+     <APIProvider apiKey={"AIzaSyAsH8omDk8y0lSGLTW9YtZiiQ2MkmsF-uQ"}>
+    <Map
+      zoom={12}
+      center={{lat: centerCords?centerCords.center_lat:51.50853, lng: centerCords?centerCords.center_lon:-0.12574}}
+      gestureHandling={'greedy'}
+      
+      // disableDefaultUI={true}
+    >
+      {pins?.map((key,value)=>{
+
+        const cords = key.location_coord.split(" , ");
+        console.log("coordinates",Number(cords[1]));
+        return(
+        <Marker ref={markerRef} position={{lat: Number(cords[0]), lng: Number(cords[1])}} />)
+      })}
+      {/* <Circle /> */}
+      {/* <Marker ref={markerRef}  position={{lat: 51.5380341, lng: -0.1023212}}   /> */}
+    </Map>
+  </APIProvider>
+
     </div>
   );
 }
