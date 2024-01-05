426e47b87c16cf20a7241864112452610168902d
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-01-05 09:31:30 +0100

    REMOVED UNNECESSARY IMPORTS AND ADDED WEBSOCKETS CONFIGS

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,227 +1,62 @@
 import { useState } from "react";
 import Layout from "../Layout/Layout";
-import MySurveys from "./MySurveys";
 import MainMap from "../components/Map";
 import { useGlobalContext } from "../Context/PreviewContext";
-import FetchNearby from "../data/fetchNearby";
-import FetchPlaceDetail from "../data/fetchPlaceDetail";
-import SimpleMapPage from "../components/Clone/options_map_page";
 
 const LandingPage = () => {
   const context = useGlobalContext();
   console.log("context Value: ",context)
-  const { inputData, setInputData, setAPIKey, api_key, setCenterCoords, centerCoords, placeAPIKey } = context;
-  const [loading, setLoading] = useState(false);
-  const [receivedKey, setRecievedKey] = useState("EhdQUTM2K0hNLCBOYWlyb2JpLCBLZW55YSImOiQKCg2PPDr");
-  const [placeDetails, setPlaceDetails] = useState([]);
-  const [nearbyResults, setNearbyResults] = useState([]);
+  const { centerCoords } = context;
+  const [dataFromServer, setDataFromServer] = useState(null);
   
-  //   const { data } = useQuery({
-  //     queryFn: async () => FetchCountries(),
-  //     queryKey: 'countries'
-  // })
-  //   console.log(data)
 
-  // const validateKey = (api_key) =>{
-  //   return (api_key === "");
-  // }
-  // const saveAPIkey = async () => {
-  //   if (validateKey(api_key)){
-  //     alert("Please provide an API key")
-  //   } else if (api_key.length<36){
-  //     alert("Please input a valid API_key")
-  //   } 
-  //   else {
-  //   // alert(api_key)
-  //   setRecievedKey(true);
-  //   }
-  // }
-
-  const centerParams = {
-    center_lat: centerCoords.lat,
-    center_lon: centerCoords.lon,
-  };
-
-  // const defaultSearchOptions = {
-  //   radius1: inputData.radius1,
-  //   radius2: inputData.radius2,
-  //   center_lat: 29.40303,
-  //   center_lon: 73.60256,
-  //   query_string: inputData.query_string,
-  //   limit: "60",
-  //   api_key: "EhdQUTM2K0hNLCBOYWlyb2JpLCBLZW55YSImOiQKCg2PPDr",
+  // const centerParams = {
+  //   center_lat: centerCoords.lat,
+  //   center_lon: centerCoords.lon,
   // };
 
 
-  const handleSearch = async () => {
-    if (!isValidInput(inputData)) {
-      alert("please fill all the fields");
-      return;
-    }
-    if (Number(inputData.radius1)>Number(inputData.radius2)){
-      alert("'From' has to be larger than 'To'");
-      return;
-    }
-    try {
+  // instance of websocket connection as a state
+  const [ws, setWs] = useState(new WebSocket('ws://localhost:3000/ws'));
 
-        setLoading(true);
+  const updateLocation =(event)=>{
+    setDataFromServer(event)
+  }
+  ws.addEventListener("open", (event) => {
+    socket.send("Hello Server!");
+  })
+  ws.addEventListener("message", (event) => {
+    updateLocation(event)
+    
+  })
 
-      //Try commenting this so that only one 'search option' is defined. 
-      const searchOptions = {
-        radius1: inputData.radius1,
-        radius2: inputData.radius2,
-        center_lat: centerCoords.lat,
-        center_lon: centerCoords.lon,
-        query_string: inputData.query_string,
-        limit: "60",
-        api_key: placeAPIKey,
-      };
-      const response = await FetchNearby(searchOptions)
-      console.log("response_data",response.data.place_id_list)
-      setNearbyResults(response.data.place_id_list)
-      console.log("coordinates used for the search are", centerCoords.lat,centerCoords.lon)
-      console.log("nearby", nearbyResults);
-      if (response.data.place_id_list?.length>0) {
-        // setPlaceIds(nearbyResults.data.place_id_list);
-        const placeDetailOptions = {
-          place_id_list: response.data.place_id_list,
-          center_loc: "",
-          api_key:placeAPIKey,
-        };
-        const placeDetail = await FetchPlaceDetail(placeDetailOptions);
-        setPlaceDetails(placeDetail.data.succesful_results);
-      }
-    } catch (error) {
-      // Handle errors
-      console.error("Error:", error);
-    } finally {
-      console.log("finally block executed")
-      setLoading(false);
-      
-    }
+  // Error handling
+  ws.addEventListener('error', (error) => {
+    console.error('WebSocket error:', error);
+    // Handle error state or reconnect logic here
+  });
+
+  // Close WebSocket connection on component unmount
+  ws.onclose = () => {
+    console.log('WebSocket connection closed');
   };
 
-  const isValidInput = (inputData) => {
-    // Implement your input validation logic here
-    return (
-      inputData.country !== "" &&
-      inputData.city !== "" &&
-      inputData.radius1 !== "" &&
-      inputData.radius2 !== "" &&
-      inputData.query_string !== ""
-    );
-  };
 
   return (
-    (receivedKey != "ENTER API KEY")?
     <Layout>
-      <main className="w-[91%]  h-full mb-10">
-        <MySurveys loading={loading} />
-        {/* <MainMap/> */}
-        <div className="px-4 md:px-10 mt-[40px] md:pl-[100px]">
-        <div className="w-full flex"> 
-          <div>
-            {/* {console.log("pageDetails",nearbyResults)} */}
-            {placeDetails.length>0?<MainMap centerCords={ {
-            lat:centerCoords.lat,
-            lng:centerCoords.lon
-              }}  pins = {placeDetails}/>:<MainMap  pins = {null}/>}
+      <main className="w-full  h-full ">
 
+        <div className="flex mx-12 my-12 h-[390px]"> 
+          <div className="w-[900px]">
+           <MainMap  pins = {null}/>
 
-            
-            {console.log("Place Details",placeDetails)}
-
-            <p  className="h-[390px] lg:w-[400px] xl:w-[650px] " />
           </div>
-            <div className="w-[320px] ml-[25px]  ">
-              <div className="w-full bg-[#7ED957] h-[270px]">
-                <p className="  h-[40px] text-white px-4 text-center font-semibold text-[20px]">
-                  Your Selection
-                </p>
-                <div className="bg-[#D2E5D1] h-[230px]">
-                  <ul className="w-full h-full px-4 list-disc ml-[20px]  py-[10px]">
-                    <li className="text-[16px] font-medium mt-[7px]">
-                      Country - {inputData.country}
-                    </li>
-                    <li className="text-[16px] font-medium mt-[7px]">
-                      Location - {inputData.city}
-                    </li>
-                    <li className="text-[16px] font-medium mt-[7px]">
-                      Distance from center - between {inputData.radius1}-
-                      {inputData.radius2} meters
-                    </li>
-                    <li className="text-[16px] font-medium mt-[7px]">
-                      Category - {inputData.query_string}
-                    </li>
-                    <li className="text-[16px] font-medium mt-[7px]">
-                      Search Limit - 500 NOS
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="w-full text-white flex justify-between mt-[10px]">
-                <button className="h-[50px] bg-[#7ED957] w-[100px]">
-                  Download
-                </button>
-                <button className="h-[50px] bg-[#7ED957] w-[60px]">
-                  Email
-                </button>
-                <button className="h-[50px] bg-[#7ED957] w-[80px]">
-                  Refresh
-                </button>
-              </div>
-              <button
-                className="w-full text-white font-semibold bg-[#128437] h-[50px] mt-[10px]"
-                onClick={handleSearch}
-                disabled={loading}
-              >
-                {loading ? "Loading..." : "Search"}
-              </button>
-            </div>
-          </div>
+         
 
-          <div className="w-full h-full px-4 bg-[#D3FFE6] mt-6 pb-6">
-            <p className="text-[18px] font-bold pt-[10px]">
-              {placeDetails.length} search results
-            </p>
-            <div className="w-full md:grid md:grid-cols-2 lg:grid-cols-3 grid-flow-dense gap-2 ">
-              {console.log("placedetails",placeDetails)}
-              {placeDetails.map((product) => {
-                const {
-                  placeId,
-                  photo_reference,
-                  address,
-                  website,
-                  place_name,
-                } = product;
-                return (
-                  <div
-                    key={placeId}
-                    className="w-[270px] md:w-[180px] lg:w-[200px] xl:w-[280px] 2xl:w-[300px] mt-[30px] h-[300px] bg-white rounded-[10px] text-black"
-                  >
-                    <img
-                      src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=358&photo_reference=${photo_reference}&key=AIzaSyAsH8omDk8y0lSGLTW9YtZiiQ2MkmsF-uQ`}
-                      alt="image"
-                      className="rounded-t-[10px] w-full h-[150px]"
-                    />
-                    <div className="px-1">
-                      <p className="font-semibold text-[18px]">{place_name}</p>
-                      <p className="font-medium">{address}</p>
-                      <p className="truncate">{website}</p>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+         
         </div>
       </main>
-    </Layout>:
-    <div class="flex justify-center mt-60 items-center bg-white ">
-        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 grid-cols-12">
-              <h3>Enter your API Key</h3>
-        </div>
-    </div>
+    </Layout>
   );
 };
 
